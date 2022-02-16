6f07d44c4b59cc93fb0f50adf57e1a1535a67e0d
shiyangtaodh <shiyangtao@dhcc.com.cn>
2022-02-16 14:25:50 +0800

    20220216_1425

---
+++ src/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public String name;
+    public Integer age;
+    public boolean sex;
+
+}
